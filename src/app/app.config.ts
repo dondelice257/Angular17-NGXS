737d481a714424e85357cb471fe0ea22e319c3c4
dondelice257 <dondelicedushime@gmail.com>
2024-03-29 00:18:12 +0200

    add NgxsStoragePluginModule to save datas in store when the user refreshes the page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,12 +12,26 @@ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 //state
 import { PostState } from './components/store/posts.state';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 
 // we import the ngxs modules to the prividers injected on the config
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(NgxsModule.forRoot([PostState], {developmentMode: !environment.production})),
+    importProvidersFrom(
+      NgxsModule.forRoot([PostState], 
+      {developmentMode: !environment.production}),
+
+      //*I added this module to make sure the datas in store are saved even when the user refreshes the page
+      //* if you want save other datas on page refreshing please add the state name below in this array
+      NgxsStoragePluginModule.forRoot({
+        key: [
+            'posts',
+  
+        ],
+    }),
+
+    ),
     importProvidersFrom(NgxsReduxDevtoolsPluginModule.forRoot({
       disabled: environment.production
     })),
